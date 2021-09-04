015bb01ceed9c31fc65e37cd2740da572de5f92f
arturpezda <atipezda@gmail.com>
2021-09-04 13:41:55 +0200

    CHORE: cleared structure CHORE: added nuxt-property-decorator FEAT: created blank Tile component

---
+++ components/Tile.vue
@@ -0,0 +1,18 @@
+<template>
+  <br/>
+</template>
+
+<script lang='ts'>
+import {
+  Component,
+  Vue
+} from 'nuxt-property-decorator'
+
+@Component
+export default class Tile extends Vue {
+
+}
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Tutorial/>
+  <Tile/>
 </template>
 
 <script lang="ts">
